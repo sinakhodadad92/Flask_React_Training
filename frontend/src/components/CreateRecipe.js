6dbcd5ade57d6547c3aa8a3fb448fd02d6ab84ba
sinakhodadad92@gmail.com <sinakhodadad92@gmail.com>
2024-07-03 10:39:11 +0200

    Part 13 Creating API Resources

--- frontend/src/components/CreateRecipe.js
+++ frontend/src/components/CreateRecipe.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const CreateRecipe = () => {
+
+    const { register, handleSubmit, reset, formState:{errors} } = useForm();
+
+    const createRecipe = (data) => {
+
+        const token = localStorage.getItem("REACT_TOKEN_AUTH_KEY");
+        
+        const requestOptions = {
+            method: "POST",
+            headers: { 
+                "content-type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(token)}`
+
+            },
+            body: JSON.stringify(data)
+        }
+
+        fetch("/api/recipes", requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        }).catch(error => {
+            console.error("There was an error", error);
+        });
+    }
+
     return (
         <div className="container">
-            <h1>Create Recipe</h1>
+            <h1>Create a Recipe</h1>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" {...register("title", { required: true, maxLength: 25 })} />
+                </Form.Group>
+                <br />
+                <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={5} placeholder="Enter description" {...register("description", { required: true, maxLength: 100 })} />
+                </Form.Group>
+                {errors.title && <p style={{color: "red"}}><small>Title is required!</small></p>}
+                {errors.title?.type === "maxLength" && <p style={{color: "red"}}><small>Title cannot exceed 25 characters</small></p>}
+                <br />
+                <Button variant="primary" type="submit" onClick={handleSubmit(createRecipe)}>
+                    Submit
+                </Button>
+            </Form>
         </div>
     );
 }
--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Recipe = ({title, description}) => {
     return (
-        <div className="recipe">
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </div>
+        <Card className="recipe">
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
